11cbebb0baf4b2c81f363c92b0757c877ae68860
Duong Tran <cvinta@gmail.com>
2020-07-23 16:46:31 +0700

    fix

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,12 +2,8 @@ import React from "react";
 import BookCard from "./BookCard";
 import "./Book.css";
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 import { BACKEND_URL } from "../app.constant";
 
-=======
-import { BACKEND_URL } from "../app.constants";
->>>>>>> develop
 export default function BookList() {
   const [bookList, setbookList] = useState([]);
 
